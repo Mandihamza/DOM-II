aef3ac8e5c09b23d41259f199a1c11692f0bdfe1
mandihamza <mandihamza@gmail.com>
2019-04-13 16:39:50 -0600

    Added style and hover effects to nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const navLinks = document.querySelectorAll('.nav-link')
+console.log(navLinks);
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('mouseover', function(event){
+      event.target.classList.add('nav-active');
+  });
+  navLink.addEventListener('mouseout', function(event){
+  event.target.classList.remove('nav-active')
+})
+});
